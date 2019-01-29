6747d17f15d6a4b454e65d01789a79ad6528afa1
Sylwester Fraczek <sylwester.fraczek@intel.com>
2019-01-29 15:55:42 +0100

    CalculateScales finished for minmax variant

--- paddle/fluid/inference/analysis/quantizator.cc
+++ paddle/fluid/inference/analysis/quantizator.cc
@@ -55,7 +55,8 @@ bool Quantizator::GatherData() {
 }
 
 void Quantizator::CalculateScales(std::string op_name, std::string conn_name,
-                                  std::string var_name, LoDTensor &lod_tensor) {
+                                  std::string var_name, LoDTensor &lod_tensor,
+                                  float int_max_value) {
   // adds pairs variable name -> LoDTensor with scale to the scales map
 
   using contrib::QuantizeAlgorithm;
@@ -63,20 +64,28 @@ void Quantizator::CalculateScales(std::string op_name, std::string conn_name,
 
   LoDTensor scale_tensor;
   scale_tensor.Resize(1);
-  // auto *tensor_ptr = scale_tensor.mutable_data<float>(CPUPlace);
-  auto eigen_tensor = EigenVector<float>::From(scale_tensor);
+  if (lod_tensor.numel() == 0)
+    throw std::runtime_error(
+        "Quantizator: LoDTensor of variable for quantization should not be "
+        "empty.");
+  auto eigen_data_vector = EigenVector<float>::From(lod_tensor);
 
   auto &rule = config_->rules_[op_name][conn_name];
   switch (rule) {
     case QuantizeAlgorithm::none:
       return;
     case QuantizeAlgorithm::minmax:
-      max_value = eigen_tensor.lpNorm<Eigen::Infinity>();
+      auto tensor_max_value = eigen_data_vector.lpNorm<Eigen::Infinity>();
+      auto quantization_factor = int_max_value / tensor_max_value;
+      scale_tensor.mutable_data<float>(CPUPlace)[0] = quantization_factor;
       break;
     case QuantizeAlgorithm::KL:
+      throw std::runtime_error(
+          "Quantizator: QuantizeAlgorithm KL is not yet implemented.");
       break;
     default:
-      throw std::runtime_error("Unknown QuantizeAlgorithm for quantization.");
+      throw std::runtime_error(
+          "Quantizator: Unexpected QuantizeAlgorithm specified.");
   }
   scales[var_name] = std::move(scale_tensor);
 }
@@ -107,7 +116,7 @@ bool Quantizator::Quantize() {
   // gather data from variables
   if (!GatherData()) return false;
   // calculate scales
-  if (!CalculateScales()) return false;
+  // if (!CalculateScales()) return false;
   // run quantization pass
   // run optimization passes
   // save quantized model if required
