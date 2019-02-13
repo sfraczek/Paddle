22f8116a53e6ac422c2d29f4caf80e6b96d2105a
Sylwester Fraczek <sylwester.fraczek@intel.com>
2019-02-13 16:46:41 +0100

    updated copyright to quantizer

--- paddle/fluid/inference/analysis/quantizer.h
+++ paddle/fluid/inference/analysis/quantizer.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 PaddlePaddle Authors. All Rights Reserved.
+// Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,14 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-/*
- * This file defines IRPassManager, it helps control the passes in IR. Inference
- * phrase will load the model program and parameters from disk, that is quite
- * different from the training phase.
- * This manager will control the Passes and make the passes in IR work smoothly
- * for inference.
- */
 
 #pragma once
 
